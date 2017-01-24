3a3da4900d567353c4286a889b722693f8979f36
Justin Rhude <justin.rhude52@gmail.com>
2017-01-24 17:03:41 -0500

    Initialized project files with a hello world program.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftwareEngineeringProject
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+        }
+    }
+}
