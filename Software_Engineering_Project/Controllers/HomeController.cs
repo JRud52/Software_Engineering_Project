5d331fc3eabdc5c179a6f071b6cc9eb8b60c5d86
Justin Rhude <justin.rhude52@gmail.com>
2017-03-12 01:44:43 -0500

    added connection string to web.config, but connection is still a WIP

--- Software_Engineering_Project/Controllers/HomeController.cs
+++ Software_Engineering_Project/Controllers/HomeController.cs
@@ -30,7 +30,24 @@ namespace Software_Engineering_Project.Controllers
         [HttpPost]
         public ActionResult LogOn(Models.Users user)
         {
-        
+           // System.Diagnostics.Debug.WriteLine("");
+            System.Configuration.Configuration rootWebConfig =
+                System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("/Software_Engineering_Project");
+            System.Configuration.ConnectionStringSettings connString;
+            if (rootWebConfig.ConnectionStrings.ConnectionStrings.Count > 0)
+            {
+                connString =
+                    rootWebConfig.ConnectionStrings.ConnectionStrings["db"];
+                if (connString != null)
+                    System.Diagnostics.Debug.WriteLine("Connection string = \"{0}\"",
+                        connString.ConnectionString);
+                else
+                    System.Diagnostics.Debug.WriteLine("No connection string");
+            }
+
+
+
+
             // show the passed data in a seperate page.
             return Content("Username " + user.email + " <br/>Password: " + user.hash); //View(user);
         }
