373c2fc373a7e56de55315bc0b5e7ce975f324f7
Guy C <guy.coccimiglio@gmail.com>
2017-03-13 22:39:23 -0400

    Added test Controller and Model

---
+++ Software_Engineering_Project/Models/RoomsContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Software_Engineering_Project.Models
+{
+    public class RoomsContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public RoomsContext() : base("name=RoomsContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Software_Engineering_Project.Models.Rooms> Rooms { get; set; }
+    }
+}
